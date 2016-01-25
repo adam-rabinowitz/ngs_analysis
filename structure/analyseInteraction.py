Fixed error in calculation of log2 directionality

structure/analyseInteraction.py
@@ -45,67 +45,25 @@ class maskMatrix(object):
         distMatrix = np.abs(centreArray - centreArray[:,None])
         self.distMatrix = ma.masked_array(distMatrix, mask = maskMatrix)
     
-    def binDirectionOld(self):
-        ''' Extract interaction direction data for bins '''
-        # Create dataframe to store data
-        df = pd.DataFrame(columns = ['self', 'inter', 'up', 'down', 'log2'])
-        self.binDF = pd.concat([self.binDF, df], axis=1)
-        # Loop through rows of the matrix
-        for rowNo, row in enumerate(self.probMatrix):
-            # Set none values if bin is entirely masked
-            if ma.count(row) == 0:
-                continue
-            # Else calculate values
-            else:
-                # Extract self frequency
-                selflig = row[rowNo]
-                # Extract up frequency
-                up = row[:rowNo]
-                if ma.count(up) == 0:
-                    up = 0.
-                else:
-                    up = up.sum()
-                # Extract down frequency
-                down = row[rowNo + 1:]
-                if ma.count(down) == 0:
-                    down = 0.
-                else:
-                    down = down.sum()
-                # Calculate inter value
-                inter = sum(row.data) - selflig - up - down
-                # Calculate log2 value
-                if up == 0:
-                    if down == 0:
-                        log2 = np.nan
-                    else:
-                        log2 = -np.inf
-                elif down == 0:
-                    log2 = np.inf
-                else:
-                    log2 = np.log2(up/down)
-                # Store results
-                self.binDF.loc[rowNo,['self','inter','up','down','log2']] = (
-                    selflig, inter, up, down, log2)
-    
     def upDown(self, array, index):
         ''' Returns distance-paired upstream and downstream arrays.'''
         if index == 0:
-            up = np.array([])
+            up = ma.masked_array([], mask=np.array([],dtype=bool))
         else:
             up = array[index-1::-1]
         down = array[index+1:]
         return(up, down)
     
-    def unmaskedPair(self, a1, a2, maxl = 10):
+    def unmaskedPair(self, a1, a2, maxl):
         ''' Returns indices of unmasked array pairs.'''
         maxl = min(len(a1), len(a2), maxl)
         if maxl == 0:
-            return(np.nan)
+            return(np.array([]))
         masked = np.logical_or(a1.mask[:maxl], a2.mask[:maxl])
-        indices = np.where(masked == False)
+        indices = np.where(masked == False)[0]
         return(indices) 
     
-    def binDirection(self):
+    def binDirection(self, maxl = 10):
         ''' Extract interaction direction data for bins '''
         # Create dataframe to store data
         df = pd.DataFrame(columns = ['self', 'inter', 'up', 'down', 'log2'])
@@ -119,15 +77,20 @@ class maskMatrix(object):
             else:
                 # Extract up and down arrays
                 up, down = self.upDown(row, rowNo)
-                print up, down
                 # Extract probabilities
                 selfProb = row[rowNo].sum()
-                upProb = up.sum()
-                downProb = down.sum()
+                if up.count() == 0:
+                    upProb = 0
+                else:
+                    upProb = up.sum()
+                if down.count() == 0:
+                    downProb = 0
+                else:
+                    downProb = down.sum()
                 interProb = 1 - upProb - downProb - selfProb
                 # Extract paired bins for log2 calculations
-                indices = self.unmaskedPair(up, down)
-                if indices:
+                indices = self.unmaskedPair(up, down, maxl)
+                if len(indices) > 0:
                     # Calculate sum of paired up and down bins
                     upSum = up[indices].sum()
                     downSum = down[indices].sum()
@@ -141,6 +104,8 @@ class maskMatrix(object):
                         log2 = np.inf
                     else:
                         log2 = np.log2(upSum/downSum)
+                else:
+                    log2 = np.nan
                 # Store results
                 self.binDF.loc[rowNo,['self','inter','up','down','log2']] = (
                     selfProb, interProb, upProb, downProb, log2)
@@ -161,7 +126,7 @@ class maskMatrix(object):
                 dist = ma.average(row, weights = self.probMatrix[rowNo])
                 self.binDF.loc[rowNo,'dist'] = np.uint32(dist)
     
-    def combinedDistance(self, fraction = 2/3.0):
+    def combinedDistance(self):
         ''' Extract lowess smooth interaction frequency for dataset '''
         # Extract probabilities
         prob = self.probMatrix[~self.probMatrix.mask]

structure/unittest/analyseInteractionTest.py
@@ -12,19 +12,32 @@ class InteractionTestCase(unittest.TestCase):
         ''' Create data for unittest.'''
         # Create file names
         self.dirName = tempfile.mkdtemp()
-        self.inMatrix = self.dirName + '/test.matrix'
-        # Create matrix
-        regions = ['chr1:1-10','chr1:11-41','chr1:42-49','chr2:1-21','chr2:22-30',
-            'chr2:31-46', 'chr2:47-51', 'chr2:52-63', 'chr3:1-24']
-        matrix = np.full((len(regions),len(regions)), 0.125)
-        matrix[5,:] = 0
-        matrix[:,5] = 0
-        np.savetxt(self.inMatrix, matrix, '%s', '\t', header = '\t'.join(regions),
-            comments = '')
+        self.inMatrix1 = self.dirName + '/test1.matrix'
+        self.inMatrix2 = self.dirName + '/test2.matrix'
+        # Create matrix 1
+        regions1 = ['chr1:1-10','chr1:11-41','chr1:42-49','chr2:1-21',
+            'chr2:22-30', 'chr2:31-46', 'chr2:47-51', 'chr2:52-63',
+            'chr3:1-24']
+        matrix1 = np.full((len(regions1),len(regions1)), 0.125)
+        matrix1[4,:] = 0
+        matrix1[:,4] = 0
+        np.savetxt(self.inMatrix1, matrix1, '%s', '\t',
+            header = '\t'.join(regions1), comments = '')
+        # Create matrix 2
+        regions2 = ['chr1:1-1', 'chr1:2-2', 'chr1:3-3', 'chr1:4-4', 'chr1:5-5']
+        matrix2 = np.array([
+            [0.2, 0.2, 0.1, 0.2, 0.3],
+            [0.4, 0.1, 0.2, 0.2, 0.1],
+            [0.1, 0.1, 0.4, 0.1, 0.3],
+            [0.2, 0.2, 0.2, 0.2, 0.2],
+            [0.1, 0.4, 0.1, 0.2, 0.2]
+        ])
+        np.savetxt(self.inMatrix2, matrix2, '%s', '\t',
+            header = '\t'.join(regions2), comments = '')
     
     def tearDown(self):
         ''' Remove temporary files and directories '''
-        for file in [self.inMatrix]:
+        for file in [self.inMatrix1, self.inMatrix2]:
             if os.path.isfile(file):
                 os.remove(file)
         os.removedirs(self.dirName)
@@ -45,28 +58,37 @@ class InteractionTestCase(unittest.TestCase):
 
 class TestInteractionAnalysis(InteractionTestCase):
     
-    def test_bin_directionality(self):
+    def test_dataframe_generation(self):
         ''' checking bin directionality calculation '''
-        maskMatrix = analyseInteraction.maskMatrix(self.inMatrix)
-        print '\n', maskMatrix.probMatrix
+        maskMatrix = analyseInteraction.maskMatrix(self.inMatrix1)
         maskMatrix.binDirection()
         df = maskMatrix.binDF
-        print df
         self.assertTrue(self.compna(df['group'], pd.Series(
-        ['chr1', 'chr1', 'chr1', 'chr2', 'chr2', np.nan, 'chr2', 'chr2', 'chr3'])))
+        ['chr1', 'chr1', 'chr1', 'chr2', np.nan, 'chr2', 'chr2', 'chr2', 'chr3'])))
         self.assertTrue(self.compna(df['up'], pd.Series(
-            [0, 0.125, 0.25, 0, 0.125, np.nan, 0.25, 0.375, 0])))
+            [0, 0.125, 0.25, 0, np.nan, 0.125, 0.25, 0.375, 0])))
         self.assertTrue(self.compna(df['down'], pd.Series(
-            [0.25, 0.125, 0, 0.375, 0.25, np.nan, 0.125, 0, 0])))
+            [0.25, 0.125, 0, 0.375, np.nan, 0.25, 0.125, 0, 0])))
         self.assertTrue(self.compna(df['inter'], pd.Series(
-            [0.625, 0.625, 0.625, 0.5, 0.5, np.nan, 0.5, 0.5, 0.875])))
+            [0.625, 0.625, 0.625, 0.5, np.nan, 0.5, 0.5, 0.5, 0.875])))
         self.assertTrue(self.compna(df['log2'], pd.Series(
-            [-np.inf, 0, np.inf, -np.inf, -1, np.nan, 1, np.inf, np.nan])))
+            [np.nan, 0, np.nan, np.nan, np.nan, 0, 0, np.nan, np.nan])))
 
-    def test_bin_distance(self):
-        maskMatrix = analyseInteraction.maskMatrix(self.inMatrix)
+    def test_bin_distance1(self):
+        ''' Test log2 calculation with standard window. '''
+        maskMatrix = analyseInteraction.maskMatrix(self.inMatrix2)
         maskMatrix.binDirection()
-        maskMatrix.binDistance()
+        df = maskMatrix.binDF
+        self.assertTrue(self.compna(df['log2'], pd.Series(
+            [np.nan, 1, -1, 0])))
+    
+    def test_bin_distance2(self):
+        ''' Test log2 calculation with short window. '''
+        maskMatrix = analyseInteraction.maskMatrix(self.inMatrix2)
+        maskMatrix.binDirection(maxl=1)
+        df = maskMatrix.binDF
+        self.assertTrue(self.compna(df['log2'], pd.Series(
+            [np.nan, 1, 0, 0])))
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestInteractionAnalysis)