a77c2a2c6edfd5fc75fb6233d66dbfa7735e89b2
adam-rabinowitz <adamrabs@hotmail.com>
2016-03-16 08:52:53 +0000

    Modified RNASeQC function to take single end parameter

--- bam/bamQC.py
+++ bam/bamQC.py
@@ -1,7 +1,8 @@
 import os
+from general_python import toolbox
 
 def RNASeqC(inBam, fasta, gtf, rRNA, outDir, outPrefix, seqcPath,
-    javaPath = 'java', check = True):
+    javaPath = 'java', check = True, singleEnd = False):
     ''' Function to perform RNASeqC analysis of RNA-Seq aligned BAM.
     Duplicates should be marked, but not deleted from the BAM file.
     Function takes 9 arguments.
@@ -19,20 +20,22 @@ def RNASeqC(inBam, fasta, gtf, rRNA, outDir, outPrefix, seqcPath,
 
     '''
     # Check FASTA file
-    if isinstance(check, bool):
-        if check:
-            fai = fasta + '.fai'
-            dict = fasta[:-2] + 'dict'
-            for f in [fai, dict]:
-                if not os.path.isfile(f):
-                    raise IOError('Genome file %s not found' %(f))
-    else:
-        raise IOError("'Check' argument must be boolean")
+    toolbox.checkArg(check, 'bool')
+    if check:
+        fai = fasta + '.fai'
+        dict = fasta[:-2] + 'dict'
+        for f in [fai, dict]:
+            if not os.path.isfile(f):
+                raise IOError('Genome file %s not found' %(f))
     # Create sample data string
     sampleData = "'" + outPrefix + "|" + inBam + '|' + outPrefix + "'"
     # Create command
     seqcCommand = [javaPath, '-jar', seqcPath, '-r', fasta, '-rRNA', rRNA,
         '-t', gtf, '-o', outDir, '-s', sampleData]
+    # Process singleEnd Argument
+    toolbox.checkArg(singleEnd, 'bool')
+    if singleEnd:
+        seqcCommand.append('-singleEnd')
     # Join and return command
     seqcCommand = ' '.join(seqcCommand)
     return(seqcCommand)
