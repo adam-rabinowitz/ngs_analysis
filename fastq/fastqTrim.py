8d00f348b47f28fd3c26ae6d4abbc0f333aadee6
adam-rabinowitz <adamrabs@hotmail.com>
2016-03-16 08:56:41 +0000

    Modified cutadapt trim functions to concatenate multiple FASTQ files prior to trimming

--- fastq/fastqTrim.py
+++ fastq/fastqTrim.py
@@ -1,60 +1,5 @@
-import os
-import re
-import sys
-import pysam
-import multiprocessing
-import subprocess
-import collections
-from github.general_functions import iohandle
-
-def extract_random(read1In, read2In, read1Out, read2Out, number = 100000):
-    ''' This function generates and reutrns a command to extracts random reads
-    from paired fastq files. Function takes four arguments:
-    
-    1)  read1In - Path to read1 gzipped FASTQ input file.
-    2)  read2In - Path to read2 gzipped FASTQ input file.
-    3)  read1Out - Path to read1 gzipped FASTQ output file.
-    4)  read2Out - Path to read2 gzipped FASTQ output file.
-    5)  Number of reads to output.
-    
-    '''
-    # Paste files
-    step1 = 'paste <(zcat %s) <(zcat %s)' %(
-        read1In,
-        read2In
-    )
-    # Merge lines by group of 4
-    step2 = 'awk \'{ printf("%s",$0); n++; if(n%4==0) { printf("\\n");} else'+\
-        '{ printf("\\t\\t");} }\''
-    # Shuffle file and take desired number 
-    step3 = 'shuf | head -%s' %(
-        number
-    )
-    # restore delimiter
-    step4 = 'sed \'s/\\t\\t/\\n/g\''
-    # Print output
-    #step5 = 'awk -F \'\\t\' \'{print $1 > "%s"; print $2 > "%s"}\'' %(
-    #    read1Out,
-    #    read2Out
-    #)
-    step5 = 'awk -F \'\\t\' \'{print $1 | "gzip > %s"; print $2 | "gzip > %s"}\'' %(
-        read1Out,
-        read2Out
-    )
-    # Combine commands
-    completeCommand = '%s | %s | %s | %s | %s' %(
-        step1,
-        step2,
-        step3,
-        step4,
-        step5
-    )
-    # Return command
-    return(completeCommand)
-
-
 ################################################################################
-## cutadapt_trim_paired
+## cutadaptTrimPaired
 ################################################################################
 # Define function
 def cutadaptTrimPaired(read1In, read2In, read1Out, read2Out, quality = 20,
@@ -77,21 +22,135 @@ def cutadaptTrimPaired(read1In, read2In, read1Out, read2Out, quality = 20,
     8)  Path to cutadapt; Default = 'cutadapt'.
     
     '''
+    # Check name of output files
+    if (not read1Out.endswith('.fastq.gz') or
+        not read2Out.endswith('.fastq.gz')):
+        raise IOError('Output must be gzipped FASTQ files')
     # Create temporary FASTQ files
-    tempRead1 = read1Out + '.tmp.fastq'
-    tempRead2 = read2Out + '.tmp.fastq'
+    concatRead1 = read1Out[:-9] + '.concat.fastq.gz'
+    concatRead2 = read2Out[:-9] + '.concat.fastq.gz'
+    tempRead1 = read1Out[:-9] + '.trimtmp.fastq.gz'
+    tempRead2 = read2Out[:-9] + '.trimtmp.fastq.gz'
+    # Process read1In list input
+    if isinstance(read1In, list):
+        # Check suffixes:
+        for read in read1In:
+            if not read.endswith('.fastq.gz'):
+                raise IOError('Input must be gzipped FASTQ files')
+        # Create concatenation command
+        if len(read1In) > 1:
+            concat1Command = 'zcat %s | gzip > %s' %(
+                ' '.join(read1In), concatRead1)
+            trimFile1 = concatRead1
+        else:
+            concat1Command = ''
+            trimFile1 = read1In[0]
+    # Process read1In string input
+    elif isinstance(read1In, str):
+        if not read1In.endswith('.fastq.gz'):
+            raise IOError('Input must be gzipped FASTQ files')
+        concat1Command = ''
+        trimFile1 = read1In
+    # Process read2In list input
+    if isinstance(read2In, list):
+        # Check suffixes:
+        for read in read2In:
+            if not read.endswith('.fastq.gz'):
+                raise IOError('Input must be gzipped FASTQ files')
+        # Create concatenation command
+        if len(read2In) > 1:
+            concat2Command = 'zcat %s | gzip > %s' %(
+                ' '.join(read2In), concatRead2)
+            trimFile2 = concatRead2
+        else:
+            concat2Command = ''
+            trimFile2 = read2In[0]
+    # Process read1In string input
+    elif isinstance(read2In, str):
+        if not read2In.endswith('.fastq.gz'):
+            raise IOError('Input must be gzipped FASTQ files')
+        concat2Command = ''
+        trimFile2 = read2In
     # Generate trimming commands
     trim1Command = [path, '-q', str(quality), '-a', adapter, '-e',
         '0.1', '--minimum-length', str(length), '-O', '1', '-o', tempRead1,
-        '-p', tempRead2, read1In, read2In]
+        '-p', tempRead2, trimFile1, trimFile2]
     trim2Command = [path, '-q', str(quality), '-a', adapter, '-e',
         '0.1', '--minimum-length', str(length), '-O', '1', '-o', read2Out,
         '-p', read1Out, tempRead2, tempRead1]
+    # Generate command to remove temporary files
+    removeCommand = 'rm %s %s' %(tempRead1, tempRead2)
+    if concat1Command:
+        removeCommand += ' %s' %(concatRead1)
+    if concat2Command:
+        removeCommand += ' %s' %(concatRead2)
     # Join and return command
-    trimCommand = '%s; %s; rm %s %s' %(
-        ' '.join(trim1Command),
-        ' '.join(trim2Command),
-        tempRead1,
-        tempRead2
-    )
-    return(trimCommand)
+    jointCommand = [concat1Command, concat2Command, ' '.join(trim1Command),
+        ' '.join(trim2Command), removeCommand]
+    jointCommand = filter(None, jointCommand)
+    jointCommand = " && ".join(jointCommand)
+    return(jointCommand)
+
+
+
+################################################################################
+## cutadaptTrim
+################################################################################
+# Define function
+def cutadaptTrim(
+        readIn, readOut, quality = 20, adapter = 'AGATCGGAAGAGC', length = 25,
+        path = 'cutadapt'
+    ):
+    ''' This function peforms quality trimming on a FASTQ file using the
+    cutadapt package. Function is built for version 1.7.1 of cutadapt. The
+    default adapter sequence is common to both ends of the standard
+    Illumina adapter. Output file names ending in '.gz' will cause the
+    output to be gzipped compressed. Function takes 6 arguments:
+    
+    1)  readIn - Input fastq file.
+    2)  readOut - Output fastq file.
+    3)  quality - Minimum base quality.
+    4)  adapter - Adapter sequence to remove from 3' end of reads.
+    5)  length - Minimum read length post trimming. 
+    6)  path - Path to cutadapte executable.
+    
+    '''
+    # Check name of output files
+    if not readOut.endswith('.fastq.gz'):
+        raise IOError('Output must be gzipped FASTQ file')
+    # Create temporary FASTQ file
+    concatRead = readOut[:-9] + '.concat.fastq.gz'
+    # Process readIn list input
+    if isinstance(readIn, list):
+        # Check suffixes:
+        for read in readIn:
+            if not read.endswith('.fastq.gz'):
+                raise IOError('Input must be gzipped FASTQ files')
+        # Create concatenation command
+        if len(readIn) > 1:
+            concatCommand = 'zcat %s | gzip > %s' %(
+                ' '.join(readIn), concatRead)
+            trimFile = concatRead
+        else:
+            concatCommand = ''
+            trimFile = readIn[0]
+    # Process readIn string input
+    elif isinstance(readIn, str):
+        if not readIn.endswith('.fastq.gz'):
+            raise IOError('Input must be gzipped FASTQ files')
+        concatCommand = ''
+        trimFile = readIn
+    # Generate trimming commands
+    trimCommand = [path, '-q', str(quality), '-a', adapter, '-e',
+        '0.1', '--minimum-length', str(length), '-O', '1', '-o', readOut,
+        trimFile]
+    # Generate command to remove temporary files
+    if concatCommand:
+        removeCommand = 'rm %s' %(concatRead)
+    else:
+        removeCommand = ''
+    # Join and return command
+    jointCommand = [concatCommand, ' '.join(trimCommand), removeCommand]
+    jointCommand = filter(None, jointCommand)
+    jointCommand = " && ".join(jointCommand)
+    return(jointCommand)
