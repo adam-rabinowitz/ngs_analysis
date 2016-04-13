a0886291e7a93061ae076831d3d3f0dcab6614e5
adam-rabinowitz <adamrabs@hotmail.com>
2016-04-13 08:39:56 +0100

    Completed script to concatenate illumina FASTQ fragments

--- scripts/concatIlluminaFrags.py
+++ scripts/concatIlluminaFrags.py
@@ -16,12 +16,11 @@ Options:
 import os
 import re
 import collections
-from general_functions import docopt
-from general_functions import moab
+from general_python import docopt, moab
 # Extract arguments
 args = docopt.docopt(__doc__,version = 'v1')
 # Create regular expression to find fastq files
-rePattern = re.compile('(.*?)_L\\d{3}_(R[12])_\\d{3}\\.fastq\\.gz$')
+rePattern = re.compile('(.*?_L\\d{3}_R[12])_\\d{3}\\.fastq\\.gz$')
 # Create dictionary
 fastqDict = {}
 # Extract input files
@@ -32,38 +31,56 @@ for infile in os.listdir(args['<indir>']):
         continue
     # Add files to output dictionary
     name = match.group(1)
-    read = match.group(2)
-    if name not in fastqDict:
-        fastqDict[name] = {'R1':[],'R2':[]}
-    fastqDict[name][read].append(infile)
+    path = os.path.join(args['<indir>'], infile)
+    if name in fastqDict:
+        fastqDict[name].append(path)
+    else:
+        fastqDict[name] = [path]
 # Process samples
-for sample in fastqDict:
-    # Sort and extract reads
-    fastqDict[sample]['R1'].sort()
-    R1 = fastqDict[sample]['R1']
-    fastqDict[sample]['R2'].sort()
-    R2 = fastqDict[sample]['R2']
-    # Check for pairs
-    if not args['--unpaired']:
-        print 'Yo'
+for name in fastqDict.keys():
+    # Process unpaired reads
+    if args['--unpaired']:
+        # Extract and sort reads
+        reads = fastqDict.pop(name)
+        reads.sort()
+        # Create outfile and command
+        outfile = os.path.join(args['<outdir>'], name + '.fastq.gz')
+        command = 'zcat %s | gzip > %s' %(' '.join(reads), outfile)
+        # Check for missing files
+        for count, rd in enumerate(reads):
+            # Check for missing files
+            number = int(rd[-12:][:3])
+            if (number - count) != 1:
+                raise IOError('Missing fragments found for %s' %(name))
+    # Process paired reads
+    else:
+        # Extract files
+        if name.endswith('1'):
+            read1 = fastqDict.pop(name)
+            read2 = fastqDict.pop(name[:-1] + '2')
+        else:
+            continue
+        # Sort read files
+        read1.sort()
+        read2.sort()
+        # Create outfiles
+        outfile1 = os.path.join(args['<outdir>'], name[:-1] + '1.fastq.gz')
+        outfile2 = os.path.join(args['<outdir>'], name[:-1] + '2.fastq.gz')
         # Check equal number of read1 and read2 files
-        if len(R1) != len(R2):
-            raise IOError('Unpaired reads found for %s' %(sample))
-        # Check for matching read1 and read2 files
-        for number, r1file in enumerate(R1):
-            r2file = re.sub('R1(?=_\\d{3}\\.fastq\\.gz$)', 'R2', r1file)
-            if R2[number] != r2file:
-                raise IOError('Unpaired files found for %s %s' %(r1file,
-                    r2file))
-    # Create command
-    for infiles in [R1, R2]:
-        # Create output file
-        outfile = re.sub('_\\d{3}(?=\\.fastq\\.gz$)', '', infiles[0])
-        outfile = args['<outdir>'] + outfile
-        # Append path to input files
-        infiles = [args['<indir>'] + r for r in infiles]
-        # Create command
-        command = 'zcat %s | gzip > %s' %(' '.join(infiles), outfile)
-        moabID = moab.submitjob(command, processor=2)
-        print moabID
+        if len(read1) != len(read2):
+            raise IOError('Unpaired reads found for %s' %(name))
+        for count, (R1, R2) in enumerate(zip(read1, read2)):
+            # Check pairing
+            if  R1[-12:] != R1[-12:]:
+                raise IOError('Unpaired fragments found for %s' %(name))
+            # Check for missing files
+            number = int(R1[-12:][:3])
+            if (number - count) != 1:
+                raise IOError('Missing fragments found for %s' %(name))
+        # Create commands
+        command1 = 'zcat %s | gzip > %s' %(' '.join(read1), outfile1)
+        command2 = 'zcat %s | gzip > %s' %(' '.join(read2), outfile2)
+        moab.submitJob(command1)
+        moab.submitJob(command2)
+        print '%s paired files concatenated to prefix %s' %(len(read1), outfile1[:-10])
         
