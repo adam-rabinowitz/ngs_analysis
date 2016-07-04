0fd3d4d469f83261c86c91c7627fb935b4ba6e6b
adam-rabinowitz <adamrabs@hotmail.com>
2016-07-04 10:44:14 +0100

    Completed script to perform rsem alignment

--- scripts/RNASeq/rsemAlign.py
+++ scripts/RNASeq/rsemAlign.py
@@ -3,15 +3,18 @@
 Usage:
     
     trimFastq.py <samplename> <inprefix> <outdir> <index>
-        [--bowtie2=<bowtie2>] [--rsem=<rsem>] [--threads=<threads>] [--singleend]
-        [--forprob=<forprob>]
+        [--bowtie2=<bowtie2>] [--rsem=<rsem>] [--threads=<threads>] 
+        [--forprob=<forprob>] [--singleend] [--markdup] [--genomebam]
     
 Options:
     
     --bowtie2=<bowtie2>  Path to bowtie2 [default: bowtie2]
-    --forprob=<forprob>  Forward probability [default: 0.5]
     --rsem=<rsem>        Path to rsem-calculate-expression [default: rsem-calculate-expression]
+    --forprob=<forprob>  Forward probability [default: 0.5]
     --threads=<threads>  Number of threads [default: 4]
+    --singleend          Only single-end sequencing performed
+    --genomebam          Generate genome bam
+    --markdup            Mark duplicates on genome bam
     --help               Output this message
     
 """
@@ -23,13 +26,15 @@ from general_python import docopt, toolbox, moab
 args = docopt.docopt(__doc__,version = 'v1')
 args['--threads'] = int(args['--threads'])
 args['--forprob'] = float(args['--forprob'])
-toolbox.checkArg(args['--forprob'], 'num', mn = 0, mx = 1)
+toolbox.check_var(args['--forprob'], 'num', mn = 0, mx = 1)
 inDir, inPrefix = os.path.split(args['<inprefix>'])
 outDir = os.path.join(args['<outdir>'], args['<samplename>'])
 if not os.path.isdir(outDir):
     os.mkdir(outDir)
 outPrefix = os.path.join(outDir, args['<samplename>'])
 outLog = outPrefix + '.rsem.log'
+# Create job dictionary
+
 # Extract fastq files and generate output file names
 read1, read2 = fastqFind.findFastq(prefix = inPrefix, dirList = [inDir],
     pair = True, gzip = True)
@@ -37,7 +42,8 @@ rsemCommand = fastqAlign.rsemBowtie2Align(index = args['<index>'],
     outPrefix = outPrefix, read1 = read1, read2 = read2,
     rsemPath = args['--rsem'], bowtie2Path = args['--bowtie2'],
     threads = args['--threads'], forProb = args['--forprob'],
-    genomeBam = False, estimateRspd = False, check = True)
+    genomeBam = args['--genomebam'], estimateRspd = False, check = True)
+print rsemCommand
 jobID = moab.submitJob(rsemCommand, processor = args['--threads'],
     stdout = outLog, stderr = outLog)
 print args['<samplename>'], jobID
