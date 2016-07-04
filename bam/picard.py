52272ec5bca1ec37bd02f3517b77cf15265ec0e9
adam-rabinowitz <adamrabs@hotmail.com>
2016-07-04 10:12:46 +0100

    Created function to add or replace read group

--- bam/picard.py
+++ bam/picard.py
@@ -52,7 +52,68 @@ def markDuplicates(
         duplicateCommand += ' && rm %s.ba?*' %(inBam[:-4])
     # Return command
     return(duplicateCommand)
-    
+
+def addReplaceReadGroup(
+        inBam, outBam, name, library, platform, barcode, picardPath,
+        javaPath = 'java', group = '1', delete = True
+    ):
+    # Create command
+    rgCommand = [javaPath, '-jar', picardPath, 'AddOrReplaceReadGroups',
+        'I=' + inBam, 'O=' + outBam, 'RGID=' + group, 'RGLB=' + library,
+        'RGPL=' + 'platform', 'RGPU=' + barcode, 'RGSM=' + name]
+    # Merge command and add deletion if required
+    rgCommand = ' '.join(rgCommand)
+    if delete:
+        rgCommand += ' && rm {}'.format(inBam)
+    # Return command
+    return(rgCommand)
+
+def sortSam(
+        inBam, outBam, picardPath, javaPath = 'java', nameSort = False,
+        delete = True
+    ):
+    # Process sort type
+    if nameSort:
+        sort = 'queryname'
+    else:
+        sort = 'coordinate'
+    # Create sort command
+    sortCommand = [javaPath, '-jar', picardPath, 'SortSam', 'I=' + inBam,
+        'O=' + outBam, 'SORT_ORDER=' + sort]
+    # Merge command and add deletion if required
+    sortCommand = ' '.join(sortCommand)
+    if delete:
+        sortCommand += ' && rm {}'.format(inBam)
+    # Return command
+    return(sortCommand)
+
+def sort_addrg_mdup(
+        inBam, outBam, group, name, library, platform, barcode,
+        picardPath, javaPath = 'java', removeDuplicates = False
+    ):
+    # Create output file names
+    if not outBam.endswith('.bam'):
+        raise ValueError("Output file must have '.bam' suffix")
+    sortBam = outBam[:-4] + '.sort.bam'
+    rgBam = outBam[:-4] + '.rg.bam'
+    mdupLog = outBam[:-4] + '.dedup.log'
+    # Create seperate commands
+    sortCommand = sortSam(inBam = inBam, outBam = sortBam,
+        picardPath = picardPath, javaPath = javaPath, nameSort = False,
+        delete = True)
+    rgCommand = addReplaceReadGroup(inBam = sortBam, outBam = rgBam,
+        name = name, library = library, platform = platform, barcode = barcode,
+        picardPath = picardPath, javaPath = javaPath, group = group,
+        delete = True)
+    mdupCommand = markDuplicates(
+        inBam = rgBam, outBam = outBam, logFile = mdupLog,
+        picardPath = picardPath, javaPath = javaPath,
+        removeDuplicates = removeDuplicates, delete = True
+    )
+    # Concatenate commands and return
+    jointCommand = ' && '.join([sortCommand, rgCommand, mdupCommand])
+    return(jointCommand)
+
 def collectInsertSizeMetrics(
         inBam, outPrefix, picardPath, javaPath = 'java'
     ):
