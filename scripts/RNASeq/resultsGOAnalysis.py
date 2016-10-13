97b9102ff1cc9dc0ddabc28cfa49f801373d0107
adam-rabinowitz <adamrabs@hotmail.com>
2016-10-13 11:29:01 +0100

    Created scripts to perform GO analysis using gmt files and DESeq output

---
+++ scripts/RNASeq/resultsGOAnalysis.py
@@ -0,0 +1,60 @@
+"""resultsGOAnalysis.py
+
+Usage:
+    
+    resultsGOAnalysis.py <results> <geneCol> <statCol> <statMax> <gmt>
+        <outFile> [--minGO=<minGO>] [--maxGO=<maxGO>] [--minGene=<minGene>]
+        [--log2Col=<log2Col>] [--onlyAnno] [--noHeader] [--sepPosNeg]
+    
+Options:
+    
+    --maxGO=<maxGO>      Maximum genes in GO set [default: 1000].
+    --minGO=<minGO>      Minimum genes in GO set [default: 5].
+    --minGene=<minGene>  Minimum significant genes in GO set [default: 3].
+    --log2Col=<log2Col>  Column for log2 fold change data. Supplying this
+        results in positive and negative fold change genes being considered
+        seperately.
+    --onlyAnno           Only consider genes with annotation.
+    --noHeader           Results file has no header.
+    
+"""
+# Import required modules
+from ngs_python.gtf import gene_conversion
+from general_python import docopt
+# Extract arguments
+args = docopt.docopt(__doc__,version = 'v1')
+args['<geneCol>'] = int(args['<geneCol>'])
+args['<statCol>'] = int(args['<statCol>'])
+args['<statMax>'] = float(args['<statMax>'])
+args['--minGO'] = int(args['--minGO'])
+args['--maxGO'] = int(args['--maxGO'])
+if args['--log2Col'] is not None:
+    args['--log2Col'] = int(args['--log2Col'])
+# Parse gmt
+geneAnno = gene_conversion.parse_gmt(args['<gmt>'])
+if isinstance(args['--log2Col'], int):
+    # Extract gene list
+    allGenes, posGenes, negGenes = gene_conversion.extract_gene_results_posneg(
+        results=args['<results>'], geneCol=args['<geneCol>'],
+        log2Col=args['--log2Col'], statCol=args['<statCol>'],
+        statMax=args['<statMax>'], header=not(args['--noHeader'])
+    )
+    # Perform annotation
+    goData = gene_conversion.calculate_hypergeo_posneg(
+        allGenes=allGenes, posGenes=posGenes, negGenes=negGenes,
+        geneAnno=geneAnno, annoGenesOnly=args['--onlyAnno']
+    )
+else:
+    # Extract gene list
+    allGenes, sigGenes = gene_conversion.extract_gene_results(
+        results=args['<results>'], geneCol=args['<geneCol>'],
+        statCol=args['<statCol>'], statMax=args['<statMax>'],
+        header=not(args['--noHeader'])
+    )
+    # Perform annotation
+    goData = gene_conversion.calculate_hypergeo(
+        allGenes=allGenes, sigGenes=sigGenes, geneAnno=geneAnno,
+        annoGenesOnly=args['--onlyAnno']
+    )
+print(goData)
+#goDa`ta.to_csv(args['<outFile>'], sep='\t', index=False)
