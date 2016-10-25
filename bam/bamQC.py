a003b0c331bb57d476a7f07274016cea0c357b10
adam-rabinowitz <Adam.Rabinowitz@crick.ac.uk>
2016-10-25 17:51:50 +0100

    Modified RNASeqC command to accept memory argument

--- bam/bamQC.py
+++ bam/bamQC.py
@@ -2,40 +2,48 @@ import os
 from general_python import toolbox
 
 def RNASeqC(inBam, fasta, gtf, rRNA, outDir, outPrefix, seqcPath,
-    javaPath = 'java', check = True, singleEnd = False):
+    javaPath = 'java', check = True, singleEnd = False, memory=None):
     ''' Function to perform RNASeqC analysis of RNA-Seq aligned BAM.
     Duplicates should be marked, but not deleted from the BAM file.
     Function takes 9 arguments.
-
-    1)  inBam - Input BAM file with duplicates marked.
-    2)  fasta - FASTA file containing genome sequence.
-    3)  gtf - GTF file locating transcripts within genome.
-    4)  rRNA - Interval list file locating rRNA genes.
-    5)  outDir - Path to output directory.
-    6)  outPrefix - Prefix to use for output files.
-    7)  seqcPath - Path to RNASeQC.jar
-    8)  javaPath - Path to java executable
-    9)  check - Boolean indicating whther to check for required FASTA
-        file indices and dictionaries.
-
+    
+    Args:
+        inBam(str) - Full path to input BAM file.
+        fasta (str)- Full path to genome FASTA file.
+        gtf (str)- Full path to GTF file.
+        rRNA (str)- Full path to interval list file of rRNA exons.
+        outDir (str)- Full path to output directory.
+        outPrefix (str)- Prefix to use for output files.
+        seqcPath (str)- Path to RNASeQC.jar
+        javaPath (str)- Path to java executable
+        check (bool)- Whether to check for required FASTA
+            file indices and dictionaries.
+        singleEnd (bool)- Whether library is single end.
+        memory (int)- Memory to use in gigabytes.
+    
     '''
     # Check FASTA file
-    toolbox.check_var(check, 'bool')
+    if not isinstance(check, bool):
+        raise TypeError('check argument must be bool')
     if check:
-        fai = fasta + '.fai'
-        dict = fasta[:-2] + 'dict'
-        for f in [fai, dict]:
+        for f in [fasta, gtf, rRNA, fasta + '.fai', fasta[:-2] + 'dict']:
             if not os.path.isfile(f):
-                raise IOError('Genome file %s not found' %(f))
+                raise IOError('File {} not found'.format(f))
     # Create sample data string
     sampleData = "'" + outPrefix + "|" + inBam + '|' + outPrefix + "'"
     # Create command
     seqcCommand = [javaPath, '-jar', seqcPath, '-r', fasta, '-rRNA', rRNA,
         '-t', gtf, '-o', outDir, '-s', sampleData]
     # Process singleEnd Argument
-    toolbox.check_var(singleEnd, 'bool')
+    if not isinstance(singleEnd, bool):
+        raise TypeError('singleEnd argument must be bool')
     if singleEnd:
         seqcCommand.append('-singleEnd')
+    # Process memory argument
+    if memory:
+        if not isinstance(memory, int):
+            raise TypeError('memory argument must be integer')
+        seqcCommand.insert(1, '-Xmx{}g'.format(memory))
     # Join and return command
     seqcCommand = ' '.join(seqcCommand)
     return(seqcCommand)
