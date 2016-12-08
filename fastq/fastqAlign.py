fba5f65b38592171eac8d402f1d16c8e3b328c29
adam-rabinowitz <Adam.Rabinowitz@crick.ac.uk>
2016-12-08 08:52:22 +0000

    Removed typo in bwaMemAlign command

--- fastq/fastqAlign.py
+++ fastq/fastqAlign.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from ngs_python.bam import samtools
-from general_python import toolbox
 
 def bowtie2Align(
         index, outFile, read1, read2 = None, bowtie2Path = 'bowtie2',
@@ -26,7 +25,8 @@ def bowtie2Align(
     
     '''
     # Check for index extensions
-    toolbox.check_var(check, 'bool')
+    if not isinstance(check, bool):
+        raise TypeError('check argument must be bool')
     if check:
         suffixes = ['.1.bt2', '.2.bt2', '.3.bt2', '.4.bt2', '.rev.1.bt2',
             '.rev.2.bt2']
@@ -34,21 +34,31 @@ def bowtie2Align(
             if not os.path.isfile(index + s):
                 raise IOError('Index file %s no found' %(index + s))
     # Check and process discordant
-    toolbox.check_var(discordant, 'bool')
+    if not isinstance(discordant, bool):
+        raise TypeError('discordant argument must be bool')
     if discordant:
         discordant = ''
     else:
         discordant = '--no-discordant'
     # Check mixed
-    toolbox.check_var(mixed, 'bool')
+    if not isinstance(mixed, bool):
+        raise TypeError('mixed argument must be bool')
     if mixed:
         mixed = ''
     else:
         mixed = '--no-mixed'
     # Check upto argument
-    toolbox.check_var(upto, 'int', mn=1)
+    if not upto is None:
+        if not isinstance(upto, int):
+            raise TypeError('upto argument must be integer')
+        if upto < 1:
+            raise ValueError('upto argument must be >= 1')
     # Check maximum insert argument
-    toolbox.check_var(maxInsert, 'int', mn = 1)
+    if not maxInsert is None:
+        if not isinstance(maxInsert, int):
+            raise TypeError('maxInsert argument must be integer')
+        if maxInsert < 1:
+            raise ValueError('maxInsert argument must be >= 1')
     # Check outut file name and generate intermediate file names
     if outFile.endswith('.sam'):
         outSam = outFile
@@ -57,15 +67,15 @@ def bowtie2Align(
         outBam = outFile
         outSam = outFile[:-4] + '.sam'
     else:
-        raise ValueError("'ouFile' argument must end '.sam' or '.bam'")
+        raise ValueError("'outFile' argument must end '.sam' or '.bam'")
     # Join multiple fastq files
     if isinstance(read1, list):
         read1 = ','.join(read1)
     if isinstance(read2, list):
         read2 = ','.join(read2)
     # Create initial command
-    bowtie2Command = [bowtie2Path, '--phred33', '--very-sensitive', mixed, discordant, '-p',
-        str(threads), '-x', index, '-S', outSam]
+    bowtie2Command = [bowtie2Path, '--phred33', '--very-sensitive', mixed,
+        discordant, '-p', str(threads), '-x', index, '-S', outSam]
     # Extend command depending on if read2 is applied
     if read2:
         bowtie2Command.extend(['-1', read1, '-2', read2])
@@ -104,7 +114,7 @@ def bowtie2Align(
 
 def bwaMemAlign(
         index, outFile, read1, read2 = None, bwaPath = 'bwa', threads = 1,
-        readGroup = 1, sampleName = None, libraryID = None, platform = None,
+        readGroup = '1', sampleName = None, libraryID = None, platform = None,
         markSecondary = True, check = True, samtoolsPath = 'samtools',
         memory = '2', nameSort = False
     ):
@@ -114,35 +124,43 @@ def bwaMemAlign(
     ends with '.sam' a sam file is returned. Function takes the following 14
     arguments:
     
-    1)  index - Path and prefix of BWA index.
-    2)  outFile - Full name of output file. Must end '.sam' or '.bam'.
-    3)  read1 - Read1 FASTQ file.
-    4)  read2 - Read2 FASTQ file.
-    5)  bwaPath - Full path to BWA exectuable.
-    6)  threads - Number of threads to use.
-    7)  sampleName - Name of sample to be used in header.
-    8)  libraryID - Name of sample to be used in header.
-    9)  readGroup - Name of read group to use in header and alignments.
-    10) markSecondary - Boolean, whether to mark secondary alignments.
-    11) check - Boolean, whether to check for index extensions.
-    12) samtoolsPath - Full path to samtools executable.
-    13) memory - Memory, in G, to use in generating BAM file.
-    14) nameSort - Boolean, whether to generate a name sorted BAM file.
+    Args:
+        index (str)- Full path BWA index prefix.
+        outFile (str)- Full path to output sam or bam file.
+        read1 (str)- Read1 FASTQ file.
+        read2 (str)- Read2 FASTQ file.
+        bwaPath (str)- BWA exectuable.
+        threads (int)- Number of threads to use.
+        readGroup (str)- Read group to be used in SAM/BAM.
+        sampleName (str)- Name of sample to be used in header.
+        libraryID (str)- Library ID to be used in SAM/BAM.
+        platform (str)- Platform to be used in SAM/BAM.
+        markSecondary (bool)- Mark secondary alignments.
+        check (bool)- Check for index extensions and output directory.
+        samtoolsPath (str)- Samtools executable.
+        memory (int)- Gigabytes of memory to use in generating BAM file.
+        nameSort (bool)- Generate a name sorted BAM file.
+    
+    Returns:
+        bwaCommand (str)- Command to perform BWA alignment.
+    
+    Raises:
+        IOError - If index suffixes or output directory are absent.
+        TypeError - If arguments are of the wrong type.
+        ValueError - If arguments have an unexpected value.
     
     '''
-    # Check index extensions if requested
-    toolbox.check_var(check, 'bool')
+    # Check index extensions and output directory, if required
+    if not isinstance(check, bool):
+        raise TypeError('check argument must be bool')
     if check:
         suffixes = ['.amb', '.ann', '.bwt', '.pac', '.sa']
         for s in suffixes:
             if not os.path.isfile(index + s):
                 raise IOError('Genome index file %s no found' %(index + s))
-    # Process secondary command
-    toolbox.check_var(markSecondary, 'bool')
-    if markSecondary:
-        markSecondary = '-M'
-    else:
-        markSecondary = ''
+        if not os.path.isdir(os.path.dirname(outFile)):
+            raise IOError('Could not find output directory {}'.format(
+                os.path.dirname(outFile)))
     # Check outut file name and generate intermediate file names
     if outFile.endswith('.sam'):
         outSam = outFile
@@ -151,7 +169,14 @@ def bwaMemAlign(
         outBam = outFile
         outSam = outFile[:-4] + '.sam'
     else:
-        raise ValueError("'ouFile' argument must end '.sam' or '.bam'")
+        raise ValueError('outFile argument must end .sam or .bam')
+    # Process secondary command
+    if not isinstance(markSecondary, bool):
+        raise TypeError('markSecondary argument must be bool')
+    if markSecondary:
+        markSecondary = '-M'
+    else:
+        markSecondary = ''
     # Create command
     bwaCommand = [bwaPath, 'mem', markSecondary ,'-t', str(threads),
         index, read1, read2]
@@ -173,16 +198,13 @@ def bwaMemAlign(
         bwaCommand.insert(2,'-R')
     # Complete BWA command
     bwaCommand = '%s > %s' %(' '.join(bwaCommand), outSam)
-    # Supplement BWA command with sort command
+    # Supplement BWA command with sort command, if required, and return
     if outBam:
         sortCommand = samtools.sort(inFile = outSam, outFile = outBam,
             name = nameSort, memory = memory, delete = True,
             path = samtoolsPath, threads = threads)
-        completeCommand = bwaCommand + ' && ' + sortCommand
-    else:
-        completeCommand = bwaCommand
-    # Return complete command
-    return(completeCommand)
+        bwaCommand = bwaCommand + ' && ' + sortCommand
+    return(bwaCommand)
 
 def rsemBowtie2Align(
         index, outPrefix, read1, read2 = None,
@@ -190,25 +212,35 @@ def rsemBowtie2Align(
         forProb = 0.5, genomeBam = True, estimateRspd = True, check = True
     ):
     ''' Function generates and returns a command to align paired FASTQ
-    files to a Bowtie2 indexed RSEM refrence transcriptome. Command
-    takes 10 arguments:
+    files to a Bowtie2 indexed RSEM refrence transcriptome.
     
-    1)  index - Full path and prefix of transcriptome index.
-    2)  outPrefix - Full path and prefix of output files.
-    3)  read1 - Read1 FASTQ file(s); can be file or list of files.
-    4)  read2 - Read2 FASTQ file(s); can be file or list of files.
-    5)  rsemPath - Full path to rsem-calculate-expression binary
-    6)  bowtie2Path - Path to directory containing bowtie2 binaries
-    7)  threads - Number of threads to use in alignment
-    8)  forProb - Probabiliy that the first read is derived from the
-        forward strand of the transcript.
-    9)  genomeBam - Boolean, whether to generate a genome BAM file.
-    10) esitmateRspd - Boolean, estimate read start position distribution.
-    11) check - Boolean, check if correctindex suffixes are present
+    Args:
+        index (str)- Full path to transcriptome index prefix.
+        outPrefix (str)- Full path for output files.
+        read1 (str/list)- Full path to read1 FASTQ file or list of paths.
+        read2 (str/list)- Full path to read2 FASTQ file or list of paths.
+        rsemPath (str)- Full path to rsem-calculate-expression executable.
+        bowtie2Path (str)- Full path to directory containing bowtie2
+            executables.
+        threads (int)- Number of threads to use in alignment
+        forProb (int/float)- Probabiliy that the first read is derived from
+            the forward strand of the transcript.
+        genomeBam (bool)- Generate a genome BAM file.
+        esitmateRspd (bool)- Estimate read start position distribution.
+        check (bool)- Check if index files are present.
+    
+    Returns:
+        rsemCommand (str)- Command to perform RSEM alignment
+    
+    Raises:
+        TypeError - If arguments are of the wrong type.
+        IOError - If index files or output directory are absent.
+        ValueError - If arguments are of unexpected values.
     
     '''
-    # Check for correct suffixes
-    toolbox.check_var(check, 'bool')
+    # Check for correct suffixes and output directory
+    if not isinstance(check, bool):
+        raise TypeError('check argument must be bool')
     if check:
         suffix = [
             '.grp', '.ti', '.transcripts.fa', '.seq', '.idx.fa', '.1.bt2',
@@ -216,17 +248,21 @@ def rsemBowtie2Align(
         ]
         for s in suffix:
             if not os.path.isfile(index + s):
-                raise IOError('Could not find index file %s' %(
-                index + s
-            ))
+                raise IOError('Could not find index file {}'.format(
+                    index + s))
+        if not os.path.isdir(os.path.dirname(outPrefix)):
+            raise IOError('Could not find output directory {}'.format(
+                os.path.dirname(outPrefix)))
     # Process genomeBAM argument
-    toolbox.check_var(genomeBam, 'bool')
+    if not isinstance(genomeBam, bool):
+        raise TypeError('genomeBam argument must be bool')
     if genomeBam:
         genomeBam = '--output-genome-bam'
     else:
         genomeBam = ''
     # Process estimateRspd argument
-    toolbox.check_var(estimateRspd, 'bool')
+    if not isinstance(estimateRspd, bool):
+        raise TypeError('estimateRspd argument must be bool')
     if estimateRspd:
         estimateRspd = '--estimate-rspd'
     else:
@@ -234,8 +270,13 @@ def rsemBowtie2Align(
     # Process read arguments
     if isinstance(read1, list):
         read1 = ','.join(read1)
-    if isinstance(read2, list):
-        read2 = ','.join(read2)
+    if not isinstance(read1, str):
+        raise TypeError('read1 must be string or list of strings')
+    if not read2 is None:
+        if isinstance(read2, list):
+            read2 = ','.join(read2)
+        if not isinstance(read2, str):
+            raise TypeError('read2 must be string or list of strings')
     # Build command
     rsemCommand = [rsemPath, '-p', str(threads), '--bowtie2',
         '--bowtie2-sensitivity-level', 'very_sensitive', genomeBam,
@@ -256,7 +297,7 @@ def rsemBowtie2Align(
 def tophat2Align(
         genomeIndex, transcriptIndex, outDir, read1, read2 = None,
         path = 'tophat', forProb = 0.5, mateDist = 50, mateSD = 20,
-        threads = 1, sampleName = '', libraryID = '', readGroup = '',
+        threads = 1, sampleName = None, libraryID = None, readGroup = None,
         discordant = True, mixed = True, check = True
     ):
     ''' Function to generate and return a command to run a tophat2
@@ -283,9 +324,10 @@ def tophat2Align(
     
     '''
     # Check for suffixes to transcriptome and genome indices
-    toolbox.check_var(check, 'bool')
+    if not isinstance(check, bool):
+        raise TypeError('check argument must be bool')
     if check:
-        # Check for Bowtie2 Genome indeces
+        # Check for Bowtie2 Genome indices
         suffix = ['.1.bt2', '.2.bt2', '.3.bt2', '.4.bt2', '.rev.1.bt2',
             '.rev.2.bt2', '.fa']
         for s in suffix:
@@ -298,7 +340,10 @@ def tophat2Align(
                     transcriptIndex + s
                 ))
     # Process forProb argument
-    toolbox.check_var(forProb, 'num', mn = 0, mx = 1)
+    if not isinstance(forProb, (float, int)):
+        raise TypeError('forProb argument must be numeric')
+    if not 1 >= forProb >= 0:
+        raise ValueError('forProb argument must be >= 0 and <= 1')
     if forProb == 1:
         forProb = 'fr-secondstrand'
     elif forProb == 0:
@@ -306,13 +351,15 @@ def tophat2Align(
     else:
         forProb = 'fr-unstranded'
     # Process discordant
-    toolbox.check_var(discordant, 'bool')
+    if not isinstance(discordant, bool):
+        raise TypeError('discordant argument must be bool')
     if discordant:
         discordant = ''
     else:
         discordant = '--no-discordant'
     # Process mixed
-    toolbox.check_var(mixed, 'bool')
+    if not isinstance(mixed, bool):
+        raise TypeError('mixed argument must be bool')
     if mixed:
         mixed = ''
     else:
@@ -320,38 +367,49 @@ def tophat2Align(
     # Process read arguments
     if isinstance(read1, list):
         read1 = ','.join(read1)
-    toolbox.check_var(read1, 'str')
+    if not isinstance(read1, str):
+        raise TypeError('read1 argument must be string or list of strings')
     if isinstance(read2, list):
         read2 = ','.join(read2)
-    toolbox.check_var(read2, 'str')
+    if not isinstance(read2, str):
+        raise TypeError('read2 argument must be string or list of strings')
+    # Check threads argument
+    if not isinstance(threads, int):
+        raise TypeError('threads argument must be integer')
+    if not 32 >= threads >= 1:
+        raise ValueError('threads argument must be >= 1 and <= 32')
     # Build tophat2 command
     tophatCommand = [path, '-o', outDir, '-p', str(threads),
         '--transcriptome-index', transcriptIndex, discordant,
         mixed, '--library-type', forProb, '--keep-fasta-order',
         '--b2-very-sensitive', genomeIndex, read1, read2]
     # Process mate arguments
-    if isinstance(mateDist, str):
-        if not mateDist.startswith('$'):
-            raise ValueError()
-    else:
-        toolbox.check_var(mateDist, 'int', mn = -100, mx = 5000)
-    if isinstance(mateSD, str):
-        if not mateDist.startswith('$'):
-            raise ValueError()
-    else:
-        toolbox.check_var(mateSD, 'int', gt = 0, mx = 5000)
+    if not isinstance(mateDist, int):
+        raise TypeError('mateDist argument must be integer')
+    if not -100 <= mateDist <= 5000:
+        raise ValueError('mateDist argument must be >= -100 and <= 5000')
+    if not isinstance(mateSD, int):
+        raise TypeError('mateSD argument must be integer')
+    if not 10 <= mateSD <= 5000:
+        raise ValueError('mateSD argument must be >= 10 and <= 5000')
     if read2:
         mateArgument = ['--mate-inner-dist', str(mateDist),
             '--mate-std-dev', str(mateSD)]
         tophatCommand = tophatCommand[:7] + mateArgument + tophatCommand[7:]
     # Add read group information
-    if sampleName:
+    if not sampleName is None:
+        if not isinstance(sampleName, str):
+            raise TypeError('sampleName argument must be string')
         tophatCommand.insert(-3, '--rg-sample')
         tophatCommand.insert(-3, sampleName)
-    if libraryID:
+    if not libraryID is None:
+        if not isinstance(libraryID, str):
+            raise TypeError('libraryID argument must be string')
         tophatCommand.insert(-3, '--rg-library'), 
         tophatCommand.insert(-3, libraryID)
-    if readGroup:
+    if not readGroup is None:
+        if not isinstance(readGroup, str):
+            raise TypeError('readGroup argument must be string')
         tophatCommand.insert(-3, '--rg-id',)
         tophatCommand.insert(-3, readGroup)
     # Concatenate and return command
