28e206a3ae3b3e066982c59770c534ea9f6e6f14
adam-rabinowitz <Adam.Rabinowitz@crick.ac.uk>
2017-02-01 15:23:49 +0000

    added function findFastqMultiPrefix to find FASTQ files with multiple prefixes

--- fastq/fastqFind.py
+++ fastq/fastqFind.py
@@ -70,6 +70,90 @@ def findFastq(prefix, dirList, pair = True):
     else:
         return(read1)
 
+def findFastqMultiPrefix(prefixList, dirList, pair = True):
+    ''' A function to identify R1 and R2 FASTQ files from directories
+    using a supplied list of FASTQ prefixes.
+    
+    Args:
+        prefixList - A tuple or list of file prefixes.
+        dirList - A tuple or list of directories to search.
+        pair (bool)- Whether to return only paired reads. Paired
+            reads must be in the same directory. Default = True.
+    
+    Returns:
+        fastqDict - A dictionary where the key is the prefix and the
+            value lists the FASTQ files. Where pair is True value is a
+            two element list of lists where the first element is read1
+            files and the second element is read2 files. Where pair is
+            False the value is simply a list of read1 files.
+    
+    '''
+    # Check pair argument
+    if not isinstance(prefixList, (tuple, list)):
+        raise TypeError('prefixList must be tuple or list')
+    if not isinstance(dirList, (tuple, list)):
+        raise TypeError('dirList must be tuple or list')
+    if not isinstance(pair, bool):
+        raise ValueError("'pair' argument must be boolean")
+    # Create dictionary to store Fastq files
+    fastqDict = {}
+    for prefix in prefixList:
+        if pair:
+            fastqDict[prefix] = [[],[]]
+        else:
+            fastqDict[prefix] = []
+    # Create regular expressions for each prefix
+    regxDict = {}
+    for prefix in prefixList:
+        escPrefix = re.escape(prefix)
+        read1Pattern = re.compile(
+            escPrefix+'.*?R1(_\\d{3}){0,1}\\.fastq(\\.gz){0,1}$')
+        regxDict[prefix] = read1Pattern
+    # Create regular expression for read2
+    regxRead2 = re.compile('R1(?=(_\\d{3}){0,1}\\.fastq(\\.gz){0,1}$)')
+    # Loop through directories to find fastq files
+    for directory in dirList:
+        # Extract file names
+        for (dirpath, dirnames, filenames) in os.walk(directory):
+                # Loop through filenames
+                for f in filenames:
+                    # Loop through prefixes
+                    for prefix in regxDict:
+                        # Find files matching read1 regular expression
+                        if regxDict[prefix].match(f):
+                            # Store read name
+                            read1 = os.path.join(dirpath, f)
+                            # Process pairs
+                            if pair:
+                                # Generate name of paired read file
+                                read2, nsub = regxRead2.subn('R2', read1)
+                                # Raise error if read2 filename not generated
+                                if not nsub == 1:
+                                    raise IOError('Could not generate read2 '\
+                                        'for {}'.format(read1))
+                                # Check for existence of paired read file
+                                if not os.path.isfile(read2):
+                                    raise IOError('Could not find {}'.format(
+                                        read2))
+                                # Store files
+                                fastqDict[prefix][0].append(read1)
+                                fastqDict[prefix][1].append(read2)
+                            # Store singletons
+                            else:
+                                fastqDict[prefix].append(read1)
+    # Sort and return data
+    for prefix in fastqDict:
+        if pair:
+            for count, fastqFiles in enumerate(fastqDict[prefix]):
+                fastqFiles = list(set(fastqFiles))
+                fastqFiles.sort()
+                fastqDict[prefix][count] = fastqFiles
+        else:
+            fastqFiles = fastqDict[prefix]
+            fastqFile = list(set(fastqFiles))
+            fastqFiles.sort()
+    return(fastqDict)
+
 def findIlluminaFastq(prefix, dirList, pair = True):
     ''' A function to identify R1 and R2 FASTQ files from directories
     using a supplied filename prefix. Function returns two list
