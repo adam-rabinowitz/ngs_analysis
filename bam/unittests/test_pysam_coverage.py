0572ab16acb861cd7b2857bfa19b591669ddaef3
adam-rabinowitz <Adam.Rabinowitz@crick.ac.uk>
2017-03-15 11:33:52 +0000

    Added supplementary alignments to unittests

--- bam/unittests/test_pysam_coverage.py
+++ bam/unittests/test_pysam_coverage.py
@@ -121,7 +121,162 @@ class test_coverage_histogram(unittest.TestCase):
         covhist = self.cov.coverage_histogram([('ref', 0, 70)], max_cov=3)
         exphist = np.array([1, 0.828, 0.542, 0.4])
         self.assertTrue(np.allclose(covhist, exphist, rtol=0, atol=0.001))
+
+class test_create_bins(unittest.TestCase):
     
+    def setUp(self):
+        dirpath = os.path.dirname(os.path.realpath(__file__))
+        bamfile = os.path.join(dirpath, 'test_coverage.bam')
+        self.cov = pysam_coverage.single_coverage(bamfile)
+    
+    def test_equal_multiple(self):
+        bindict = self.cov.create_bins(10, True)
+        expstart = np.array([0, 10, 20, 30, 40, 50, 60])
+        expend = np.array([10, 20, 30, 40, 50, 60, 70])
+        self.assertTrue(np.all(bindict['ref']['start'] == expstart))
+        self.assertTrue(np.all(bindict['ref']['end'] == expend))
+        
+    def test_equal_remainder(self):
+        bindict = self.cov.create_bins(9, True)
+        expstart = np.array([3, 12, 21, 30, 39, 48, 57])
+        expend = np.array([12, 21, 30, 39, 48, 57, 66])
+        self.assertTrue(np.all(bindict['ref']['start'] == expstart))
+        self.assertTrue(np.all(bindict['ref']['end'] == expend))
+    
+    def test_equal_toobig(self):
+        bindict = self.cov.create_bins(71, True)
+        self.assertEqual(bindict['ref']['start'].size, 0)
+        self.assertEqual(bindict['ref']['end'].size, 0)
+    
+    def test_unequal_multiple(self):
+        bindict = self.cov.create_bins(10, False)
+        expstart = np.array([0, 10, 20, 30, 40, 50, 60])
+        expend = np.array([10, 20, 30, 40, 50, 60, 70])
+        self.assertTrue(np.all(bindict['ref']['start'] == expstart))
+        self.assertTrue(np.all(bindict['ref']['end'] == expend))
+        
+    def test_unequal_remainder1(self):
+        bindict = self.cov.create_bins(9, False)
+        expstart = np.array([0, 9, 18, 27, 36, 45, 54, 62])
+        expend = np.array([9, 18, 27, 36, 45, 54, 62, 70])
+        self.assertTrue(np.all(bindict['ref']['start'] == expstart))
+        self.assertTrue(np.all(bindict['ref']['end'] == expend))
+        
+    def test_unequal_remainder2(self):
+        bindict = self.cov.create_bins(20, False)
+        expstart = np.array([0, 18, 36, 53])
+        expend = np.array([18, 36, 53, 70])
+        self.assertTrue(np.all(bindict['ref']['start'] == expstart))
+        self.assertTrue(np.all(bindict['ref']['end'] == expend))
+    
+    def test_unequal_toobig(self):
+        bindict = self.cov.create_bins(71, False)
+        expstart = np.array([0])
+        expend = np.array([70])
+        self.assertTrue(np.all(bindict['ref']['start'] == expstart))
+        self.assertTrue(np.all(bindict['ref']['end'] == expend))
+
+class test_add_bin_count(unittest.TestCase):
+    
+    def setUp(self):
+        dirpath = os.path.dirname(os.path.realpath(__file__))
+        bamfile = os.path.join(dirpath, 'test_coverage.bam')
+        self.cov = pysam_coverage.single_coverage(bamfile)
+    
+    def test_outside_5prime(self):
+        bindict = self.cov.create_bins(9, True)
+        self.cov.add_bin_count(bindict, 'ref', 2)
+        expcount = np.array([0, 0, 0, 0, 0, 0, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+        
+    def test_outside_3prime(self):
+        bindict = self.cov.create_bins(9, True)
+        self.cov.add_bin_count(bindict, 'ref', 66)
+        expcount = np.array([0, 0, 0, 0, 0, 0, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+
+    def test_boundary_5prime(self):
+        bindict = self.cov.create_bins(9, True)
+        self.cov.add_bin_count(bindict, 'ref', 29)
+        expcount = np.array([0, 0, 1, 0, 0, 0, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+        
+    def test_boundary_3prime(self):
+        bindict = self.cov.create_bins(9, True)
+        self.cov.add_bin_count(bindict, 'ref', 30)
+        expcount = np.array([0, 0, 0, 1, 0, 0, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+       
+class test_count_bin_overlaps(unittest.TestCase):
+
+    def setUp(self):
+        dirpath = os.path.dirname(os.path.realpath(__file__))
+        bamfile = os.path.join(dirpath, 'test_coverage.bam')
+        self.cov = pysam_coverage.single_coverage(bamfile)
+    
+    def test_all_counts_refstart(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='refstart', rmDup=False,
+            rmSec=False, rmSup=False)
+        expcount = np.array([2, 1, 3, 1, 0, 1, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_readstart(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='readstart',
+            rmDup=False, rmSec=False, rmSup=False)
+        expcount = np.array([2, 1, 3, 1, 0, 0, 1])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_refend(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='refend',
+            rmDup=False, rmSec=False, rmSup=False)
+        expcount = np.array([0, 1, 2, 0, 4, 0, 1])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_readend(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='readend',
+            rmDup=False, rmSec=False, rmSup=False)
+        expcount = np.array([0, 1, 2, 0, 4, 1, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_mapq10(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=10, overlap='refstart',
+            rmDup=False, rmSec=False, rmSup=False)
+        expcount = np.array([2, 1, 3, 1, 0, 0, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_mapq20(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=20, overlap='refstart',
+            rmDup=False, rmSec=False, rmSup=False)
+        expcount = np.array([2, 1, 3, 0, 0, 0, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_rmdup(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='refstart', rmDup=True,
+            rmSec=False, rmSup=False)
+        expcount = np.array([2, 1, 2, 1, 0, 1, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_rmsec(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='refstart', rmDup=False,
+            rmSec=True, rmSup=False)
+        expcount = np.array([1, 1, 3, 1, 0, 1, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+    
+    def test_all_counts_rmsup(self):
+        bindict = self.cov.count_bin_overlaps(
+            binSize=10, binEqual=True, mapQ=0, overlap='refstart', rmDup=False,
+            rmSec=False, rmSup=True)
+        expcount = np.array([1, 1, 3, 1, 0, 1, 0])
+        self.assertTrue(np.all(bindict['ref']['count'] == expcount))
+
 if __name__ == '__main__':
     
     suite = unittest.TestLoader().loadTestsFromTestCase(test_mean_coverage_each)
@@ -131,4 +286,13 @@ if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite)
     
     suite = unittest.TestLoader().loadTestsFromTestCase(test_coverage_histogram)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_create_bins)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_add_bin_count)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_count_bin_overlaps)
     unittest.TextTestRunner(verbosity=2).run(suite)
