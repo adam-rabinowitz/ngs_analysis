0fd817b42cee7d2f59a2adb09e63e9aec1f06203
adam-rabinowitz <Adam.Rabinowitz@crick.ac.uk>
2017-03-15 11:42:36 +0000

    Added functions to sort output bed files

--- scripts/ATACSeq/atacBam2Bed.py
+++ scripts/ATACSeq/atacBam2Bed.py
@@ -2,21 +2,22 @@
 
 Usage:
     
-    atacBam2Bed.py <bamfile> <outbed> [--minMapQ=<minMapQ>] [--size=<size>]
-        [--rmDup]
+    atacBam2Bed.py <bamfile> <outprefix> [--minMapQ=<minMapQ>] [--size=<size>]
+        [--sort]
     
     atacBam2Bed.py (-h | --help)
     
 Options:
     
-    --minMapQ=<minMapQ>  Minimum mapping quality of reads [default: 20]
-    --size=<size>        Size of open region around insertion [default: 50]
-    --rmDup              Flag to remove duplicate reads
-    --help               Output this message
+    --minMapQ=<minMapQ>  Minimum mapping quality of reads [default: 20].
+    --size=<size>        Size of open region around insertion [default: 50].
+    --sort               Sort bedfile. Requires bedtools to be in path.
+    --help               Output this message.
     
 """
 import pysam
 import collections
+import subprocess
 from general_python import docopt
 from ngs_python.bam import pysamfunc
 # Extract arguments
@@ -27,43 +28,52 @@ try:
 except ValueError:
     raise IOError('size argument must be an integer divisible by 2')
 if args['--size'] % 2:
-    raise IOError('size argument must be an integer divisible by 2')
+    raise IOError('size argument must be divisible by 2')
 # Process size argument
 try:
     args['--minMapQ'] = int(args['--minMapQ'])
 except ValueError:
     raise IOError('minMapQ argument must be an integer')
-# Create counter to store processing metrics
-counter = collections.defaultdict(int)
-# Open input and output files
-outBed = open(args['<outbed>'], 'w')
+# Create file names and open input files
+tempBedPath = args['<outprefix>'] + '.temp.bed'
+finalBedPath = args['<outprefix>'] + '.bed'
+tempBed = open(tempBedPath, 'w')
 inBam = pysam.AlignmentFile(args['<bamfile>'])
-# Create dictionary contianing chromosome names and lengths
-chrDict = pysamfunc.createChrDict(inBam)
+# Create variables for processing BAM files
+chrDict = pysamfunc.chr_dict(inBam)
+strDict = {True: '-', False: '+'}
+counter = collections.defaultdict(int)
 # Loop through BAM file and create BED file
 for read in inBam:
     # Count total and extract flag
     counter['total'] += 1
     flag = read.flag
+    # Count and skip unmapped reads
+    if flag & 4:
+        counter['unmapped'] += 1
+        continue
+    # Count and skip poorly mapped reads
+    if read.mapping_quality < args['--minMapQ']:
+        counter['poormap'] += 1
+        continue
+    # Count and skip reads with unmapped mate
+    if flag & 8:
+        counter['nomate'] += 1
+        continue
+    # Count and skip improper pairs
+    if not flag & 2:
+        counter['improper']
     # Count and skip duplicates
-    if args['--rmDup'] and flag & 1024:
+    if flag & 1024:
         counter['duplicate'] += 1
         continue
     # Count and skip secondary alignments
     if flag & 256:
         counter['secondary'] += 1
         continue
-    # Count and skip unmapped reads
-    if flag & 4:
-        counter['unmapped'] += 1
-        continue
-    # Count and skip reads with unmapped mate
-    if flag & 8:
-        counter['nomate'] += 1
-        continue
-    # Count and skip poorly mapped reads
-    if read.mapping_quality < args['--minMapQ']:
-        counter['poormap'] += 1
+    # Count and skip secondary alignments
+    if flag & 2048:
+        counter['supplement'] += 1
         continue
     # Count and skip reads with clipping at read start
     if flag & 16:
@@ -74,22 +84,30 @@ for read in inBam:
         if read.cigartuples[0][0] in [4, 5]:
             counter['clipped'] += 1
             continue
-    # Count accepted reads
+    # Count and write accepted reads
     counter['accepted'] += 1
-    # Extract bed data from read
-    regionTuple = pysamfunc.startInterval(read, args['--size'], chrDict)
-    # Save regions to file
-    regionTuple = '\t'.join(map(str, regionTuple))
-    outBed.write('%s\n' %(regionTuple))
-# Close files
-outBed.close()
+    positionTuple = pysamfunc.startInterval(read, args['--size'], chrDict)
+    tempBed.write('\t'.join(map(str, positionTuple)) + '\n')
+# Close input file
+tempBed.close()
 inBam.close()
-# Print metric
-print 'Total reads: %s' %(counter['total'])
-print 'Duplicates: %s' %(counter['duplicate'])
-print 'Secondary: %s' %(counter['secondary'])
-print 'Unmapped: %s' %(counter['unmapped'])
-print 'Mate unmapped: %s' %(counter['nomate'])
-print 'Poorly mapped: %s' %(counter['poormap'])
-print 'Start clipped: %s' %(counter['clipped'])
-print 'Accepted: %s' %(counter['accepted'])
+# Open log file and write
+with open(args['<outprefix>'] + '.log', 'w') as logfile:
+    logfile.write('Total: {}\n'.format(counter['total']))
+    logfile.write('Unmapped: {}\n'.format(counter['unmapped']))
+    logfile.write('Map quality <{}: {}\n'.format(
+        args['--minMapQ'], counter['poormap']))
+    logfile.write('Mate Unmapped: {}\n'.format(counter['nomate']))
+    logfile.write('Improper pair: {}\n'.format(counter['improper']))
+    logfile.write('Duplicate: {}\n'.format(counter['duplicate']))
+    logfile.write('Secondary: {}\n'.format(counter['secondary']))
+    logfile.write('Supplementary: {}\n'.format(counter['supplement']))
+    logfile.write('Start clipped: {}\n'.format(counter['clipped']))
+    logfile.write('Accepted: {}\n'.format(counter['accepted']))
+# Create and submit sort command
+if args['--sort']:
+    sortCommand = 'bedtools sort -i {} > {} && rm {}'.format(
+        tempBedPath, finalBedPath, tempBedPath)
+else:
+    sortCommand = 'mv {} {}'.format(tempBedPath, finalBedPath)
+subprocess.check_call(sortCommand, shell=True)
